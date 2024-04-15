2187c1323529374eba49d5ee25b6de6e87a5b020
WHS-DTECH <166963235+WHS-DTECH@users.noreply.github.com>
2024-04-16 11:05:58 +1200

    Create animal-details.py
    This program categorises animals via number of legs, height and whether or not it has a table.

---
+++ animal-details.py
@@ -0,0 +1,14 @@
+name="Elephant" #String
+numLegs = 4 # interger
+height = 4.5 # float
+hasTail = True # boolean
+
+print(name)
+print(numLegs)
+print(height)
+print(hasTail)
+
+print("Name: ", name)
+print("Number of legs: ", numLegs)
+print("Height in metres: ", height)
+print("Has a tail: ", hasTail)
